aab1a9704581d031af439dff1ef71680a72be434
kaique <kaique@gmail.com>
2020-12-11 13:10:12 -0300

    Adding database acces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+import model.entities.Department;
+
+public interface DepartmentDao {
+    
+    void insert(Department obj);
+    void deleteById(Integer id);
+    void update(Department obj);
+    Department findById(Integer id);
+    List<Department> findAll();
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface SellerDao {
+    
+    void insert(Seller obj);
+    void deleteById(Integer id);
+    void update(Seller obj);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
+}
